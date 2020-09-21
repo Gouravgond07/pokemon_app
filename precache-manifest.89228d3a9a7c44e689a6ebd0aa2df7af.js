72fde67cb458833867bca9b5dfefe023170fc667
Gourav <gouravgond07@gmail.com>
2020-09-21 21:25:29 +0530

    Updates

---
+++ precache-manifest.89228d3a9a7c44e689a6ebd0aa2df7af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5aa287eb14b49c534813c761a23595f",
+    "url": "/pokemon_app/index.html"
+  },
+  {
+    "revision": "5b4b578d378f38e5d075",
+    "url": "/pokemon_app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e5e4ef72cccd65f7ec2b",
+    "url": "/pokemon_app/static/css/main.e4a2aed0.chunk.css"
+  },
+  {
+    "revision": "5b4b578d378f38e5d075",
+    "url": "/pokemon_app/static/js/2.f3f38747.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon_app/static/js/2.f3f38747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e4ef72cccd65f7ec2b",
+    "url": "/pokemon_app/static/js/main.b580327f.chunk.js"
+  },
+  {
+    "revision": "87380099a12288a54d09",
+    "url": "/pokemon_app/static/js/runtime-main.301108b1.js"
+  }
+]);
